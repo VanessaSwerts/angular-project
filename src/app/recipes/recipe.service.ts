c261ebbdd8ff29a2647fdb454f7e244ad2d17500
Vanessa Swerts <vanessaswerts@gec.inatel.br>
2023-05-15 16:43:46 -0300

    Adding ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,30 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/Ingredient.model";
 
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test ', 'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1386/20210503-MLMC-Spring-Radish-Pea-Lobster-Salad-Labneh2874-1-530x380-c.jpg'),
-        new Recipe('A Test Recipe 2 ', 'This is simply a test 2', 'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1386/20210503-MLMC-Spring-Radish-Pea-Lobster-Salad-Labneh2874-1-530x380-c.jpg'),
-        new Recipe('A Test Recipe 3', 'This is simply a test 3', 'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1386/20210503-MLMC-Spring-Radish-Pea-Lobster-Salad-Labneh2874-1-530x380-c.jpg')
+        new Recipe(
+            'A Test Recipe',
+            'This is simply a test ',
+            'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1386/20210503-MLMC-Spring-Radish-Pea-Lobster-Salad-Labneh2874-1-530x380-c.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]
+        ),
+        new Recipe(
+            'A Test Recipe 2',
+            'This is simply a test ',
+            'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1386/20210503-MLMC-Spring-Radish-Pea-Lobster-Salad-Labneh2874-1-530x380-c.jpg',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 20),
+                new Ingredient('Tomate', 1)
+            ]
+        )
     ];
 
     getRecipes() {
